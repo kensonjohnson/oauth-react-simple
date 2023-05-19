f97a330b6cb21cf975f9589e5a11fe90cc1beb06
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-18 21:47:54 -0500

    Styled the output in body

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { useState } from "react";
 import { generateRandomString } from "./utils/auth";
+import { ListGroup, Button, Stack } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [state, setState] = useState(generateRandomString(64));
   return (
     <>
-      <h3>{state}</h3>
-      <button
-        onClick={() => {
-          setState(generateRandomString(64));
-        }}
-      >
-        Generate New State
-      </button>
+      <ListGroup>
+        <ListGroup.Item>
+          <Stack gap={2} direction="horizontal">
+            <div className="h-100">
+              <p className="mt-3">{state}</p>
+            </div>
+            <div className="vr" />
+            <Button
+              variant="secondary"
+              onClick={() => {
+                setState(generateRandomString(64));
+              }}
+            >
+              Generate New State
+            </Button>
+          </Stack>
+        </ListGroup.Item>
+      </ListGroup>
     </>
   );
 }
