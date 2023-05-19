fa024d4b892e41401176c587affac820bf11a150
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-18 21:16:19 -0500

    Updated random string function to accept a length

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import { generateState } from "./utils/auth";
+import { generateRandomString } from "./utils/auth";
 
 function App() {
-  const [state, setState] = useState(generateState());
+  const [state, setState] = useState(generateRandomString(64));
   return (
     <>
       <h3>{state}</h3>
       <button
         onClick={() => {
-          setState(generateState());
+          setState(generateRandomString(64));
         }}
       >
         Generate New State
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,7 @@
-export function generateState() {
+export function generateRandomString(length: number) {
   const validChars =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let array = new Uint8Array(40);
+  let array = new Uint8Array(length);
   window.crypto.getRandomValues(array);
   array = array.map((x) => {
     return validChars.codePointAt(x % validChars.length) as number;
